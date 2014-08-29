71ac2fb56f0e54747b3b93520a93eec3c3d3ff28
Marcin <marcin@Spandau.(none)>
2014-08-29 19:58:12 +0200

    Some more slots added to DB script

--- db_scripts/user_script.sql
+++ db_scripts/user_script.sql
@@ -10,4 +10,8 @@ insert into mwt_slots values (2,'13:00',null);
 insert into mwt_slots values (3,'14:00','usr1');
 insert into mwt_slots values (4,'15:00','usr2');
 insert into mwt_slots values (5,'16:00',null);
+insert into mwt_slots values (6,'17:00',null);
+insert into mwt_slots values (7,'18:00',null);
+insert into mwt_slots values (8,'19:00',null);
+insert into mwt_slots values (9,'20:00',null);
 
